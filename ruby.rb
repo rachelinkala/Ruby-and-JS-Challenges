9fd24443a94217761610216b56d0d4063c303ef3
Rachel Inkala <rachelinkala@gmail.com>
2018-08-29 14:03:58 -0600

    initial commit

---
+++ ruby.rb
@@ -0,0 +1,22 @@
+# FUNCTIONS
+
+#write a script function that returns a passed string with letters in alphabetical order
+
+#write a script that accepts a string as a parameter and converts the first letter of each word of the string in upper case
+def upper_case
+  puts 'the quick brown fox'.split.map(&:capitalize).join(' ')
+end
+
+
+# CONDITIONAL STATEMENTS AND LOOPS
+
+
+# STRINGS
+
+
+
+# ARRAY
+
+
+
+# OBJECTS
